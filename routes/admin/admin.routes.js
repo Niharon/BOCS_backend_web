2288aa2660054e879e3cbd9e8dbdae5a75bf91dc
RMasadulla <rma0011239@gmail.com>
2023-04-17 14:11:45 +0600

    topics,course-requests api done

--- routes/admin/admin.routes.js
+++ routes/admin/admin.routes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 // import the controller
 const courseController = require("../../controller/admin/course.controller");
 const lessonController = require("../../controller/admin/lesson.controller");
+const courseRequestController = require("../../controller/admin/courseRequestController");
+const topicsController = require("../../controller/admin/topic.controller");
 
+//course
 router.post("/course/create", courseController.create);
 router.patch("/course/:id", courseController.update);
 router.delete("/course/:id", courseController.delete);
@@ -13,8 +16,26 @@ router.delete("/course/:id", courseController.delete);
 router.post('/lesson/create', lessonController.createLesson);
 router.get('/lesson/total-lessions', lessonController.getAllLessons);
 router.get('/lesson/:id', lessonController.getLessonById);
-router.put('/lesson/:id', lessonController.updateLessonById);
+router.patch('/lesson/:id', lessonController.updateLessonById);
 router.delete('/lesson/:id', lessonController.deleteLessonById);
+
+
+//course-requests
+router.get('/course-request/all-course-requests', courseRequestController.getAllCourseRequests);
+router.post('/course-request/create', courseRequestController.createCourseRequest);
+router.patch('/course-request/:id', courseRequestController.updateCourseRequest);
+
+
+//topics
+router.get('/topics', topicsController.getAllTopics);
+router.get('/topics/:id', topicsController.getTopicById);
+router.post('/topics', topicsController.createTopic);
+router.patch('/topics/:id', topicsController.updateTopicById);
+router.delete('/topics/:id', topicsController.deleteTopicById);
+
+
+
+
 
 module.exports = router;
 
