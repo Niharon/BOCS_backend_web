5b7d7c12bb0da8371334c5838fe7600b73e28892
RMasadulla <rma0011239@gmail.com>
2023-04-18 09:30:07 +0600

    user login verify wiht jwt

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,55 +1,64 @@
 const User = require("../models/User.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-require('dotenv').config({path:"./config.env"});
+const crypto = require('crypto');
 
+// Generate a random 256-bit (32-byte) secret key
+const secretKey = crypto.randomBytes(32).toString('hex');
 
 exports.createUser = async (req, res,next) => {
-    try {
-        const {  email, deviceId, password } = req.body;
+  try {
+    const { email, deviceId, password } = req.body;
 
-        const salt = await bcrypt.genSalt(10);
-        const hashedPass = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
         
-        const user = await User.create({ email, password: hashedPass, deviceId});
+    const user = await User.create({ email, password: hashedPass, deviceId });
         
-        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: '1h' }); // generate JWT token
+    const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: '1h' });
         
-        res.status(201).json({ message: 'User created successfully', token: token }); // send token in response
-
-    } catch (error) {
-        res.status(400).json(error);
-    }
+    res.status(201).json({ message: 'User created successfully', token: token });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 
 exports.login = async (req, res) => {
     try {
-        const {email,password ,deviceId} = req.body;
-
-        const user = await User.findOne({ where: { email } });
-
-        if (!user) {
-            throw new Error('User not found');
-        } else {
-
-            //  first check if the device is same or not
-            if (user.deviceId !== deviceId) {
-                throw new Error('Device not matched or deviceId not provided');
-            }
-            //  now check password
-            const isPasswordValid = await bcrypt.compare(password, user.password);
-            if (!isPasswordValid) {
-                throw new Error('Invalid Credentials');
-            }
-            const token = jwt.sign({ id: user.id },process.env.SECRET_KEY, { expiresIn: '1h' }); 
-            
-            res.status(200).json({ message: 'Login successful', token: token });
-        }
+      const { email, password, deviceId } = req.body;
+  
+      const user = await User.findOne({ where: { email } });
+  
+      if (!user) {
+        throw new Error('User not found');
+      }
+      // first check if the device is same or not
+      if (user.deviceId !== deviceId) {
+        throw new Error('Device not matched or deviceId not provided');
+      }
+      // now check password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        throw new Error('Invalid Credentials');
+      }
+  
+      let token;
+      if (req.headers.authorization) {
+        token = req.headers.authorization.split(' ')[1];
+      } else {
+        token = jwt.sign({ id: user.id }, secretKey, { expiresIn: '1h' });
+      }
+  
+      const decoded = jwt.verify(token, secretKey);
+  
+      if (decoded.id !== user.id) {
+        throw new Error('Invalid token');
+      }
+  
+      res.status(200).json({ message: 'Login successful', token: token });
     } catch (error) {
-        res.status(400).json({ message: 'Login failed', error: error?.message });
+      res.status(400).json({ message: 'Login failed', error: error?.message });
     }
-};
-
-
-
+  };
+  
