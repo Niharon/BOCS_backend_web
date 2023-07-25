7e1326fbad3289291f4a7806d08a691c6144a395
Razu Ahmed Joy <rjjoybzs420@gmail.com>
2023-07-25 11:40:15 +0600

    added landing page

--- app.js
+++ app.js
@@ -12,12 +12,13 @@ app.use(express.json());
 app.use(cors());
 
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
 
-  app.use('/public',express.static(path.join(__dirname,'uploads')));
-}else{
+  app.use('/public', express.static(path.join(__dirname, 'uploads')));
+  app.use(express.static(path.join(__dirname, 'templates')));
+} else {
 
-  app.use('/public',express.static(path.join(__dirname,'uploads')));
+  app.use('/public', express.static(path.join(__dirname, 'uploads')));
 }
 
 // connect DB
@@ -26,26 +27,19 @@ db.connectDb();
 
 // routes
 
-app.get("/privacypolicy",(req,res)=>{
-  res.sendFile(path.join(__dirname,'templates/privacy_policy.html'))
+app.get("/privacypolicy", (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/privacy_policy.html'))
 })
-app.get("/termsandconditions",(req,res)=>{
-  res.sendFile(path.join(__dirname,'templates/terms_and_conditions.html'))
+app.get("/termsandconditions", (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/terms_and_conditions.html'))
 })
-app.get("/account-deletion",(req,res)=>{
-  res.sendFile(path.join(__dirname,'templates/del.html'))
+app.get("/account-deletion", (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/del.html'))
 })
-app.get("/", (req, res) => {
-  res.json({
-    message: "Welcome to the e-learning platform",
-    application_mode: process.env.NODE_ENV,
 
-
-  });
-});
 app.get("/test", (req, res) => {
- 
-  console.log(path.join(__dirname,"..", 'uploads/courses/thumbnail'))
+
+  console.log(path.join(__dirname, "..", 'uploads/courses/thumbnail'))
   res.json({
     message: "Welcome to the e-learning platform",
     application_mode: process.env.NODE_ENV,
@@ -56,6 +50,10 @@ app.get("/test", (req, res) => {
 
 app.use("/api", require("./routes"));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'templates/landing.html'))
+
+});
 
 // global error handler
 app.use(ErrorHandler);
